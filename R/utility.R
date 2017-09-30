9b6af34c3994c981b8f6715e928db83e1659e28a
Ubuntu <adevries@adv-ug-dsvm-1709.yj1dl5k52mle3ivs1pyjcebc0c.ax.internal.cloudapp.net>
2017-09-30 10:25:51 +0000

    Remove linux pipes from system commands, to ensure portability to windows

--- R/utility.R
+++ R/utility.R
@@ -1,26 +1,28 @@
 ## a constructor for named lists
 nlist <- function(...) {
-    x <- list(...)
-    names(x) <- character(0)
-    x
+  x <- list(...)
+  names(x) <- character(0)
+  x
 }
 
 ##
 ##   Not Exported Function for Testing
 ##
 sys_call <- function(cmd, args) {
-    x <- tryCatch(system2(cmd, args, stdout=TRUE, stderr=TRUE), error=function(e) NULL)
-    return(x)
+  tryCatch(system2(cmd, args, stdout=TRUE, stderr=TRUE), error=function(e) NULL)
 }
 
 pandoc_to_json <- function(file, from="markdown") {
-    args <- sprintf("-f %s -t json %s", from, file)
-    system2("pandoc", args, stdout=TRUE, stderr=TRUE)
+  args <- sprintf("-f %s -t json %s", from, file)
+  system2("pandoc", args, stdout=TRUE, stderr=TRUE)
 }
 
 pandoc_from_json <- function(json, to) {
-    args <- sprintf("%s | pandoc -f json -t %s", shQuote(json), to)
-    system2("echo", args, stdout=TRUE, stderr=TRUE)
+  tf <- tempfile(fileext = ".txt")
+  writeLines(json, tf)
+  on.exit(unlink(tf))
+  args <- sprintf("%s -f json -t %s", tf, to)
+  system2("pandoc", args, stdout=TRUE, stderr=TRUE)
 }
 
 
@@ -34,30 +36,30 @@ pandoc_from_json <- function(json, to) {
 #' @details If you want to apply a filter to the document before it get's written out, or your pandoc installation is not registered in the `PATH` it can be favorable to provide your own writer function to the document class.
 #' @export
 pandocfilters_writer <- function(x, con, format) {
-    args <- sprintf("%s | pandoc -f json -t %s", shQuote(as.character(x)), format)
-    x <- system2("echo", args, stdout=TRUE, stderr=TRUE)
-    writeLines(x, con=con)
+  args <- sprintf("%s | pandoc -f json -t %s", shQuote(as.character(x)), format)
+  x <- system2("echo", args, stdout=TRUE, stderr=TRUE)
+  writeLines(x, con=con)
 }
 
 test <- function(x, to="html") {
-    d <- list(list(unMeta=nlist()), x)
-    pandoc_from_json(as.character(jsonlite::toJSON(d, auto_unbox=TRUE)), to=to)
+  d <- list(list(unMeta=nlist()), x)
+  pandoc_from_json(as.character(jsonlite::toJSON(d, auto_unbox=TRUE)), to=to)
 }
 
 pdf_test <- function(x, to = "markdown") test(x, to)
 
 detect_pandoc_version <- function() {
-    x <- sys_call("pandoc", "--version")
-    if ( is.null(x) ) {
-        writeLines("\n\nInfo message:\nCouldn't find 'pandoc'!\nPandoc version is set to '1.16', use `set_pandoc_version` to change the settings if necessary.\n\n")
-        return(NULL)
-    }
-    b <- grepl("pandoc +\\d\\.\\d", x)  
-    if ( !any(b) ) return(NULL)
-    x <- x[b][1]
-    version <- gsub("[[:alpha:] ]", "", x)
-    version_numeric <- as.numeric(regmatches(version, regexpr("\\d+\\.\\d+", version)))
-    list(char=version, num=version_numeric)
+  x <- sys_call("pandoc", "--version")
+  if ( is.null(x) ) {
+    writeLines("\n\nInfo message:\nCouldn't find 'pandoc'!\nPandoc version is set to '1.16', use `set_pandoc_version` to change the settings if necessary.\n\n")
+    return(NULL)
+  }
+  b <- grepl("pandoc +\\d\\.\\d", x)  
+  if ( !any(b) ) return(NULL)
+  x <- x[b][1]
+  version <- gsub("[[:alpha:] ]", "", x)
+  version_numeric <- as.numeric(regmatches(version, regexpr("\\d+\\.\\d+", version)))
+  list(char=version, num=version_numeric)
 }
 ## detect_pandoc_version()
 
@@ -68,7 +70,7 @@ detect_pandoc_version <- function() {
 #' 
 #' @export
 get_pandoc_version <- function() {
-    base::getNamespace("pandocfilters")$pandoc$version
+  base::getNamespace("pandocfilters")$pandoc$version
 }
 
 
@@ -79,5 +81,5 @@ get_pandoc_version <- function() {
 #' @param x a numeric giving the pandoc version (e.g. 1.14 or 1.15 or 1.16 or 1.17)
 #' @export
 set_pandoc_version <- function(x) {    
-    assign("version", x, envir=base::getNamespace("pandocfilters")$pandoc)
+  assign("version", x, envir=base::getNamespace("pandocfilters")$pandoc)
 }
